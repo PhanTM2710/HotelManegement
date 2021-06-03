9823554d511c9bdcf14fa24c5fd4805a4ffe5e0b
phantm <phantm2710@gmail.com>
2021-06-03 09:05:20 +0700

    ShowRoomType

--- HotelManagement/src/main/java/com/projectHotel/PhanLam/controller/HomeController.java
+++ HotelManagement/src/main/java/com/projectHotel/PhanLam/controller/HomeController.java
@@ -1,18 +1,29 @@
 package com.projectHotel.PhanLam.controller;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.projectHotel.PhanLam.entity.TypeRoom;
+import com.projectHotel.PhanLam.repository.ITypeRoom;
 
 @Controller
+@RequestMapping
 public class HomeController {
 	
-	@GetMapping("/")
-	public String showHome() {
-		return "index";
+	@Autowired
+	private ITypeRoom typeroom;
+	
+	@GetMapping(value = {"/room","/"})
+	public String getListRoom(Model model) {
+		List<TypeRoom> listtypeRoom = typeroom.findAll();
+		model.addAttribute("typeroom",listtypeRoom);	
+	    return "index";
 	}
 	
-	@GetMapping(value = "/hello")
-	public String sayHello() {
-	    return "search";
-	}
 }
