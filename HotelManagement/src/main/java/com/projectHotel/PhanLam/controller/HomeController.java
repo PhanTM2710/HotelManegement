8bb408e24d3741b541ba79e11971ad9653b2d053
phantm <phantm2710@gmail.com>
2021-06-05 14:01:40 +0700

    addhtml

--- HotelManagement/src/main/java/com/projectHotel/PhanLam/controller/HomeController.java
+++ HotelManagement/src/main/java/com/projectHotel/PhanLam/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.projectHotel.PhanLam.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.projectHotel.PhanLam.entity.Service;
 import com.projectHotel.PhanLam.entity.TypeRoom;
@@ -37,4 +39,11 @@ public class HomeController {
 		model.addAttribute("listservice",listService);			
 		return "service";
 	}
+	
+	@GetMapping("/showmore")
+	@ResponseBody
+	public Optional<Service> findOne(Integer id){
+		return service.findById(id);
+	}
+	
 }
